999298db207c157260873fe77efb0cacc171e569
Bassem Fayed <bassem.fayed@gmail.com>
2022-04-29 18:15:18 +0200

    Event all created with no overlapp

--- src/Components/EventCard.js
+++ src/Components/EventCard.js
@@ -2,9 +2,36 @@ import React from 'react';
 import '../Styles/EventCard.css';
 import getPositionFromTop from '../Helpers/getPositionFromTop';
 import getDurationHeight from '../Helpers/getDurationHeight';
+import moment from 'moment';
 
 function EventCard({appointmentsData}) {
-  
+  let appointmentsDataCopy = [...appointmentsData];
+
+   // left position will be used to avoid events overlapping each other
+   function getLeftPosition(appointment){
+    const startTime = moment.duration(appointment.start).asMinutes();
+    const endTime = startTime + appointment.duration;
+    let left = 0;
+
+
+    appointmentsDataCopy.forEach((app) => {
+      if(app.id !== appointment.id){
+        const eventStartTime = moment.duration(app.start).asMinutes();
+        const eventEndTime = moment.duration(app.start).asMinutes() + app.duration;
+
+        if((startTime > eventStartTime && startTime < eventEndTime) // start of an event is in between start and end of other event
+          || (endTime < eventEndTime && endTime > eventStartTime) // end of event is in between start and end of other event
+          || (startTime === eventStartTime && endTime === eventEndTime) // start and end are exactly the same in both events
+          ){
+            left += 15
+            appointmentsDataCopy = appointmentsDataCopy.filter(element => element.id !== appointment.id) 
+          };
+      };
+    });
+
+    return left
+  };
+
   return (
       <div  style={{position: 'relative'}}>
         {appointmentsData.map((appointment) => 
@@ -12,11 +39,12 @@ function EventCard({appointmentsData}) {
               className='event' 
               key={appointment.id}
               style={{
-                height: getDurationHeight(appointment.duration) + '%', // duration of event
-                top: getPositionFromTop(appointment.start) + '%', // start of event
+                height: `${getDurationHeight(appointment.duration)}%`, // duration of event
+                top: `${getPositionFromTop(appointment.start)}%`, // start of event
+                left: `${getLeftPosition(appointment)}vw`, // position to avoid overlapping
               }}
             >
-           {appointment.id} - {appointment.start} h
+              {appointment.id} @{appointment.start}-{appointment.duration}mins
           </div>
         )}
         
