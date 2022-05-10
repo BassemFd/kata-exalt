23693f5a601f5b4ae96bfaeb4b2f15cc1918dd4c
Bassem Fayed <bassem.fayed@gmail.com>
2022-05-10 15:41:57 +0200

    small fix

--- src/Helpers/getWidthAndLeft.js
+++ src/Helpers/getWidthAndLeft.js
@@ -1,34 +1,34 @@
 import moment from 'moment';
 
-function getWidthAndLeft(input){
-  input.sort((a, b) => moment.duration(a.start).asMinutes() - moment.duration(b.start).asMinutes())
-input.forEach(appointment => {
+function getWidthAndLeft(events){
+  events.sort((a, b) => moment.duration(a.start).asMinutes() - moment.duration(b.start).asMinutes())
+events.forEach(appointment => {
   appointment.startTime = moment.duration(appointment.start).asMinutes();
   appointment.endTime = appointment.startTime + appointment.duration;
   appointment.overlapsWith = [];
   appointment.left = 0;
 });
 
-for (let i = 0; i < input.length; i++) {
-  for (let k = i + 1; k < input.length; k++) {
-      if (input[i] !== input[k]) {
-        if((input[i].startTime > input[k].startTime && input[i].endTime < input[k].endTime) 
-        || (input[i].startTime === input[k].startTime && input[i].endTime === input[k].endTime)
-        || (input[i].startTime > input[k].startTime && input[i].startTime < input[k].endTime && input[i].endTime > input[k].endTime)
-        || (input[i].startTime <= input[k].startTime && input[i].endTime > input[k].endTime)
-        || (input[i].startTime < input[k].startTime && input[i].endTime > input[k].startTime && input[i].endTime < input[k].endTime)
-        || (input[i].startTime < input[k].startTime && input[i].endTime === input[k].endTime)
+for (let i = 0; i < events.length; i++) {
+  for (let k = i + 1; k < events.length; k++) {
+      if (events[i] !== events[k]) {
+        if((events[i].startTime > events[k].startTime && events[i].endTime < events[k].endTime) 
+        || (events[i].startTime === events[k].startTime && events[i].endTime === events[k].endTime)
+        || (events[i].startTime > events[k].startTime && events[i].startTime < events[k].endTime && events[i].endTime > events[k].endTime)
+        || (events[i].startTime <= events[k].startTime && events[i].endTime > events[k].endTime)
+        || (events[i].startTime < events[k].startTime && events[i].endTime > events[k].startTime && events[i].endTime < events[k].endTime)
+        || (events[i].startTime < events[k].startTime && events[i].endTime === events[k].endTime)
         ){
-            input[k].overlapsWith.push(input[i].id)
-            input[i].overlapsWith.push(input[k].id)
-            input[i].left += 33.3;
+            events[k].overlapsWith.push(events[i].id)
+            events[i].overlapsWith.push(events[k].id)
+            events[i].left += 33.3;
 
         }
       }
     }
   };
 
-  input.map(event => {
+  events.map(event => {
     if(event.overlapsWith.length === 0){
       event.width = 100;
       event.left = 0;
@@ -38,7 +38,7 @@ for (let i = 0; i < input.length; i++) {
     }
   })
 
-return input;
+return events;
 }
 
 export default getWidthAndLeft;
