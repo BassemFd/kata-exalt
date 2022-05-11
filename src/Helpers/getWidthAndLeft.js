fe629a62615147b641d72d886773d47cf35221c5
Bassem Fayed <bassem.fayed@gmail.com>
2022-05-11 12:21:04 +0200

    width and position with for each loops

--- src/Helpers/getWidthAndLeft.js
+++ src/Helpers/getWidthAndLeft.js
@@ -9,44 +9,36 @@ function getWidthAndLeftPosition(appointmentsData){
     appointment.endTime = appointment.startTime + appointment.duration;
   });
 
-  // We sort event by event with next event by start time but also by end time, to figure out which events to group when overlapping
-  appointmentsData.sort((a, b) =>{ 
-    if (a.startTime < b.startTime || a.endTime > b.endTime){ 
-      return -1
-    } else if(a.startTime > b.startTime || a.endTime < b.endTime){
-      return 1
-    } else {
-     return 0; 
-   }
-  });
+  // We sort events by time
+      appointmentsData.sort((a, b) => b.endTime - a.endTime );
 
-  // double loop to compare all events within each other
-      for (let i = 0; i < appointmentsData.length; i++) {
-        for (let k = i; k < appointmentsData.length; k++) {
-            if (i !== k 
-              && appointmentsData[i].endTime > appointmentsData[k].startTime 
-              && appointmentsData[i].startTime < appointmentsData[k].endTime 
-              && appointmentsData[i].leftPosition === appointmentsData[k].leftPosition)
-              {
-                appointmentsData[k].leftPosition++; // position from Y axis
-              }
-            }  
-          }
+  // loop to compare all events within each other
+    appointmentsData.forEach((appointment, i) => {
+          for (let k = i; k < appointmentsData.length; k++) {
+              if (i !== k 
+                && appointment.endTime > appointmentsData[k].startTime 
+                && appointment.startTime < appointmentsData[k].endTime 
+                && appointment.leftPosition === appointmentsData[k].leftPosition)
+                {
+                  appointmentsData[k].leftPosition++; // position from Y axis
+                }
+              }  
+            })
       
     // Here we find the number of events that overlaps in same period of time so we can divide the width by number of events accordingly
-      for (let i = 0; i < appointmentsData.length; i++) {
+      appointmentsData.forEach((appointment, i) => {
         let numberOfOverlaps = 0;
         for (let k = 0; k < appointmentsData.length; k++) {
             if (i !== k 
-              && appointmentsData[i].endTime > appointmentsData[k].startTime 
-              && appointmentsData[i].startTime < appointmentsData[k].endTime 
-              && appointmentsData[i].leftPosition !== appointmentsData[k].leftPosition)
+              && appointment.endTime > appointmentsData[k].startTime 
+              && appointment.startTime < appointmentsData[k].endTime 
+              && appointment.leftPosition !== appointmentsData[k].leftPosition)
               {
-                numberOfOverlaps = Math.max(...[numberOfOverlaps, appointmentsData[i].leftPosition, appointmentsData[k].leftPosition]);
+                numberOfOverlaps = Math.max(...[numberOfOverlaps, appointment.leftPosition, appointmentsData[k].leftPosition]);
               }
             }  
-        appointmentsData[i].width = 100 / (numberOfOverlaps + 1);
-    }
+        appointment.width = 100 / (numberOfOverlaps + 1);
+    })
 
 return appointmentsData;
 }
